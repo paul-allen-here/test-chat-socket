af40cee4df2146c3fe0c88be2e5db695abe1826c
Wenomr <wenom998@gmail.com>
2020-02-18 09:17:31 +0300

    readme update

--- server/router.js
+++ server/router.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render("Server is running");
+    res.send("Server is running");
 });
 
 module.exports = router;
